63a14ee9e080926e2f98fb6074eedf0376cb532c
Seth Miller <millerado@gmail.com>
2024-01-10 13:15:25 -0700

    create main component

---
+++ src/components/Main.js
@@ -0,0 +1,20 @@
+import { Routes, Route } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+
+//make a react component which takes in a prop called playerID
+function Main(props) {
+  return (
+    <div className='Main'>
+      <header className='Main-header'>
+        <h1>Hello World</h1>
+        {/* <h2>{props.message}</h2> */}
+      </header>
+    </div>
+  );
+}
+//this component will make a fetch request to the backend
+//the backend will use axios and cheeiro to scrape the PDGA website
+//the backend will return the data to the frontend
+//the frontend will display the data
+//the data will be a list of tournaments played by the player
+//each tournament will have a link to the tournament page
