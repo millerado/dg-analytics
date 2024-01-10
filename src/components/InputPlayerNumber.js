6ad75f81a4e51921fb3f4b678e2c52ef6f8b5181
Seth Miller <millerado@gmail.com>
2024-01-10 13:49:12 -0700

    create functions to handle submitting and typing

--- src/components/InputPlayerNumber.js
+++ src/components/InputPlayerNumber.js
@@ -1,5 +1,21 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router';
+
 // make a componet called InputPlayerNumber which creates a form with a text input
 function InputPlayerNumber() {
+  const [playerNumber, setPlayerNumber] = useState('');
+
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setPlayerNumber(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    navigate(`/player/${playerNumber}`);
+  };
+
   return (
     <div className='InputPlayerNumber'>
       <form>
