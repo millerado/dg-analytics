8b7d88ae1d10773c8dfeeed121698f4565659437
Seth Miller <millerado@gmail.com>
2024-01-10 13:21:08 -0700

    dont forget to export the component

--- src/components/Main.js
+++ src/components/Main.js
@@ -6,12 +6,14 @@ function Main(props) {
   return (
     <div className='Main'>
       <header className='Main-header'>
-        <h1>Hello World</h1>
+        <h3>Hello World</h3>
         {/* <h2>{props.message}</h2> */}
       </header>
     </div>
   );
 }
+
+export default Main;
 //this component will make a fetch request to the backend
 //the backend will use axios and cheeiro to scrape the PDGA website
 //the backend will return the data to the frontend
