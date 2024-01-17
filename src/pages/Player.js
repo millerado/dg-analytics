509272763cdca464331a69b9ee0423013aabc2d9
Seth Miller <millerado@gmail.com>
2024-01-17 15:20:51 -0700

    display years active with links

--- src/pages/Player.js
+++ src/pages/Player.js
@@ -1,10 +1,20 @@
-function Player({ playerID, tournamentsPlayed }) {
+function Player({ playerID, tournaments, years }) {
   return (
     <div>
       <h1>Player: {playerID}</h1>
+      <h2>Years</h2>
+      <ul>
+        {years.map((year) => (
+          <li key={year}>
+            <a href={`http://localhost:4000/player/${playerID}/stats/${year}`}>
+              {year}
+            </a>
+          </li>
+        ))}
+      </ul>
       <h2>Tournaments</h2>
       <ul>
-        {tournamentsPlayed.map((tournament) => (
+        {tournaments.map((tournament) => (
           <li key={tournament.id}>
             <a href={tournament.link}>{tournament.name}</a>
           </li>
