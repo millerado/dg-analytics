2a2144dc71ec264e02e3972c7c843d92c090680b
Seth Miller <millerado@gmail.com>
2024-01-22 13:53:41 -0700

    remove display of played years

--- src/pages/Player.js
+++ src/pages/Player.js
@@ -1,18 +1,7 @@
-function Player({ playerID, tournaments, years }) {
+function Player({ playerID, tournaments, year }) {
   return (
     <div>
-      <h1>Player: {playerID}</h1>
-      <h2>Years</h2>
-      <ul>
-        {years.map((year) => (
-          <li key={year}>
-            <a href={`http://localhost:4000/player/${playerID}/stats/${year}`}>
-              {year}
-            </a>
-          </li>
-        ))}
-      </ul>
-      <h2>Tournaments</h2>
+      <h2>{year} Tournaments</h2>
       <ul>
         {tournaments.map((tournament) => (
           <li key={tournament.id}>
