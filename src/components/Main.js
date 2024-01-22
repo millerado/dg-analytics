1e5f4ca47061eba91815388f0c53fb940380354a
Seth Miller <millerado@gmail.com>
2024-01-22 13:54:28 -0700

    add year component and new route for other years

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,10 +3,12 @@ import { useState, useEffect } from 'react';
 
 import Player from '../pages/Player';
 import InputPlayerNumber from './InputPlayerNumber';
+import Years from './Years';
 
 function Main(props) {
   const [playerID, setPlayerID] = useState('');
   const [playerInfo, setPlayerInfo] = useState({ tournaments: [], years: [] });
+  const [year, setYear] = useState('2023');
 
   const getPlayerInfo = (playerID) => {
     try {
@@ -24,14 +26,38 @@ function Main(props) {
     }
   };
 
+  const getTournamentsByYear = (playerID, year) => {
+    try {
+      fetch(`http://localhost:4000/player/${playerID}/stats/${year}`)
+        .then((res) => res.json())
+        .then((data) =>
+          setPlayerInfo({
+            ...playerInfo,
+            ['tournaments']: data,
+          })
+        );
+    } catch (error) {
+      throw error;
+    }
+  };
+
   useEffect(() => {
     getPlayerInfo(playerID);
-  }, [playerID]);
+    getTournamentsByYear(playerID, year);
+  }, [playerID, year]);
 
   return (
     <div className='Main'>
       <div className='InputPlayerNumber'>
         <InputPlayerNumber setPlayerID={setPlayerID} playerID={playerID} />
+      </div>
+      <h1>Player: {playerID}</h1>
+      <div className='Years'>
+        <Years
+          playerID={playerID}
+          years={playerInfo['years']}
+          setYear={setYear}
+        />
       </div>
       <Routes>
         <Route
@@ -40,7 +66,17 @@ function Main(props) {
             <Player
               playerID={playerID}
               tournaments={playerInfo['tournaments']}
-              years={playerInfo['years']}
+              year={year}
+            />
+          }
+        />
+        <Route
+          path='/player/:id/stats/:year'
+          element={
+            <Player
+              playerID={playerID}
+              tournaments={playerInfo['tournaments']}
+              year={year}
             />
           }
         />
